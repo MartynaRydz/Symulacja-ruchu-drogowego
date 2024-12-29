8d0b10b14f3151d76fb1b0fbbedd4c67f3c6981e
MartynaRydz <167323126+MartynaRydz@users.noreply.github.com>
2024-12-29 20:32:02 +0100

    Add project files.

---
+++ Projekcik/CiufCiuf.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Windows.Shapes;
+
+
+namespace Projekcik
+{
+    public class CiufCiuf
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int kieruneczek { get; set; } // 1 - prawo, 0 - lewo
+        public UIElement CiufciufRectangle { get; set; }
+        public int CiufciowaPredkosc { get; set; }
+        Random random = new Random();
+
+        public CiufCiuf()
+        {
+            kieruneczek = random.Next(0, 2);
+            CiufciufRectangle = new System.Windows.Shapes.Rectangle
+            {
+                Width = 350,  // szerokość ciufci
+                Height = 150, // wysokość ciufci
+            };
+
+            if (kieruneczek == 0)
+            {
+                Y = 500;
+                X = 1200;
+                CiufciowaPredkosc = -random.Next(5, 11);
+                (CiufciufRectangle as System.Windows.Shapes.Rectangle).Fill = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTla.png"))) { Stretch = Stretch.Uniform };
+            }
+            else if(kieruneczek == 1)
+            {
+                Y = 500;
+                X = -350;
+                CiufciowaPredkosc = random.Next(2, 8);
+                (CiufciufRectangle as System.Windows.Shapes.Rectangle).Fill = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTlaPrawo.png"))) { Stretch = Stretch.Uniform };
+            }
+        }
+
+    }
+}
---
+++ Projekcik/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Drawing;
+
+namespace Projekcik
+{
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();  
+        private bool _isMoving;  //czy ciufcia się porusza
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _isMoving = false;  // Na początku ciufcia się nie porusza
+        }
+
+        private void CiufciaJedzie()
+        {
+
+            while (true)
+            {
+                CiufCiuf _ciufciuf = null;
+                int tloCiufWidth = 0;
+                int ciufciufWidth = 0;
+
+                Dispatcher.Invoke(() => 
+                {
+                    _ciufciuf = new CiufCiuf();
+                    tloCiuf.Children.Add(_ciufciuf.CiufciufRectangle);// dodajemy ciufciuf (Rectangle) na canvas
+                    tloCiufWidth = (int)tloCiuf.Width;
+                    ciufciufWidth = (int)(_ciufciuf.CiufciufRectangle as FrameworkElement).Width;
+                    Canvas.SetLeft(_ciufciuf.CiufciufRectangle, _ciufciuf.X);
+                    Canvas.SetTop(_ciufciuf.CiufciufRectangle, _ciufciuf.Y);
+                });
+
+
+                for(int i = 0; i < tloCiufWidth + ciufciufWidth*2; i += Math.Abs(_ciufciuf.CiufciowaPredkosc))
+                {
+                    _ciufciuf.X += _ciufciuf.CiufciowaPredkosc;
+                    Dispatcher.Invoke(() =>
+                    {
+                        Canvas.SetLeft(_ciufciuf.CiufciufRectangle, _ciufciuf.X);
+                        Canvas.SetTop(_ciufciuf.CiufciufRectangle, _ciufciuf.Y);
+                    });
+                    Thread.Sleep(50);
+                }
+
+                
+                Dispatcher.Invoke(()=> tloCiuf.Children.Remove(_ciufciuf.CiufciufRectangle));
+
+                switch(_random.Next(3))
+                {
+                    case 0:
+                        Thread.Sleep(5000);
+                        break;
+                    case 1:
+                        Thread.Sleep(7500);
+                        break;
+                    case 2:
+                        Thread.Sleep(10000);
+                        break;
+
+                }
+
+            }
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread wateczek = new Thread(CiufciaJedzie);
+            wateczek.Start();
+        }
+
+        
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+  
+        }
+    }
+}
