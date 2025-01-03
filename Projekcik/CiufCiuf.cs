f663b46876dd5b309d1c603071088af0b25e335a
MartynaRydz <167323126+MartynaRydz@users.noreply.github.com>
2025-01-03 22:46:48 +0100

    Update CiufCiuf.cs
    Zmiana Rectangle => Image

--- Projekcik/CiufCiuf.cs
+++ Projekcik/CiufCiuf.cs
@@ -1,48 +1,45 @@
-﻿using System;
-using System.Drawing;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Threading;
-using System.Windows.Shapes;
+using System.Windows.Controls;
 
+namespace Projekcik;
 
-namespace Projekcik
+public class CiufCiuf
 {
-    public class CiufCiuf
+    public double X { get; set; }
+    public double Y { get; set; }
+    public int Kieruneczek { get; set; } // 1 - prawo, 0 - lewo
+    public UIElement CiufciufImage { get; set; }
+    public int CiufciowaPredkosc { get; set; }
+    private Random random = new Random();
+
+    public CiufCiuf()
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public int kieruneczek { get; set; } // 1 - prawo, 0 - lewo
-        public UIElement CiufciufRectangle { get; set; }
-        public int CiufciowaPredkosc { get; set; }
-        Random random = new Random();
+        Kieruneczek = random.Next(0, 2);
 
-        public CiufCiuf()
+        // Zamiana na Image zamiast Rectangle
+        CiufciufImage = new Image
         {
-            kieruneczek = random.Next(0, 2);
-            CiufciufRectangle = new System.Windows.Shapes.Rectangle
-            {
-                Width = 350,  // szerokość ciufci
-                Height = 150, // wysokość ciufci
-            };
+            Width = 350,  // szerokość ciufci
+            Height = 150, // wysokość ciufci
+        };
 
-            if (kieruneczek == 0)
-            {
-                Y = 500;
-                X = 1200;
-                CiufciowaPredkosc = -random.Next(5, 11);
-                (CiufciufRectangle as System.Windows.Shapes.Rectangle).Fill = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTla.png"))) { Stretch = Stretch.Uniform };
-            }
-            else if(kieruneczek == 1)
-            {
-                Y = 500;
-                X = -350;
-                CiufciowaPredkosc = random.Next(2, 8);
-                (CiufciufRectangle as System.Windows.Shapes.Rectangle).Fill = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTlaPrawo.png"))) { Stretch = Stretch.Uniform };
-            }
+        if (Kieruneczek == 0)
+        {
+            Y = 500;
+            X = 1200;
+            CiufciowaPredkosc = -random.Next(5, 11);
+            (CiufciufImage as Image).Source = new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTla.png"));
+            (CiufciufImage as Image).Stretch = Stretch.Uniform;
         }
-
+        else if (Kieruneczek == 1)
+        {
+            Y = 500;
+            X = -450;
+            CiufciowaPredkosc = random.Next(2, 8);
+            (CiufciufImage as Image).Source = new BitmapImage(new Uri("pack://application:,,,/items/ciufciufBezTlaPrawo.png"));
+            (CiufciufImage as Image).Stretch = Stretch.Uniform;
+        }
     }
 }
