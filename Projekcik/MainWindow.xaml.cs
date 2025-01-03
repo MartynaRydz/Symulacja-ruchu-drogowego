6abde295bfbfafb41a32b4bd42bd00e2de2d696b
MartynaRydz <167323126+MartynaRydz@users.noreply.github.com>
2025-01-03 22:51:38 +0100

    Update MainWindow.xaml.cs
    samochód jeździ lewo prawo ale czasem nie znika pociąg

--- Projekcik/MainWindow.xaml.cs
+++ Projekcik/MainWindow.xaml.cs
@@ -1,97 +1,164 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
-namespace Projekcik
+namespace Projekcik;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private Random _random = new Random();
+    private DispatcherTimer _ciufciufTimer; //timer do obsługi ruchu ciufci
+    private CiufCiuf _ciufciuf;
+    private DispatcherTimer _samochodzikTimer; //timer do obsługi ruchu aut
+    private Samochodzik _samochodzik;
+    private bool _isMoving; //czy pojazdy jest w ruchu
+    private int _tloDrogaWidth;
+    private int _ciufciufWidth;
+    private int _samochodzikWidth;
+
+    public MainWindow()
     {
-        private Random _random = new Random();
-        private DispatcherTimer _ciufciufTimer; // Timer do obsługi ruchu ciufci
-        private CiufCiuf _ciufciuf; // Obecnie poruszająca się ciufcia
-        private bool _isMoving; // Czy ciufcia jest w ruchu
-        private int _tloCiufWidth; // Szerokość obszaru tła
-        private int _ciufciufWidth; // Szerokość ciufci
-
-        public MainWindow()
+        InitializeComponent();
+        InitializeCiufTimer();
+        InitializeSamochodzikTimer();
+        _isMoving = false;
+    }
+    #region InitializeTimer
+    private void InitializeCiufTimer()
+    {
+        //inicjalizacja timera
+        _ciufciufTimer = new DispatcherTimer
         {
-            InitializeComponent();
-            InitializeTimer();
-            _isMoving = false;
+            Interval = TimeSpan.FromMilliseconds(30)
+        };
+        _ciufciufTimer.Tick += CiufciufTimer_Tick; //obsługa zdarzenia tick
+    }
+    private void InitializeSamochodzikTimer()
+    {
+        //inicjalizacja timera
+        _samochodzikTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(30)
+        };
+        _samochodzikTimer.Tick += SamochodzikTimer_Tick; //obsługa zdarzenia tick
+    }
+    #endregion
+    #region Ciufcia
+    private void CiufciaJedzie()
+    {
+        _ciufciuf = new CiufCiuf();
+
+        tloDroga.Children.Add(_ciufciuf.CiufciufImage);
+
+        // pobranie szerokości obszaru tła i ciufci
+        _tloDrogaWidth = (int)tloDroga.Width;
+        _ciufciufWidth = (int)(_ciufciuf.CiufciufImage as FrameworkElement).Width;
+
+        // ustawienie początkowej pozycji na Canvas
+        Canvas.SetLeft(_ciufciuf.CiufciufImage, _ciufciuf.X);
+        Canvas.SetTop(_ciufciuf.CiufciufImage, _ciufciuf.Y);
+
+        _ciufciufTimer.Start();
+    }
+
+    private void CiufciufTimer_Tick(object sender, EventArgs e)
+    {
+        if (_ciufciuf == null) return;
+
+        // poruszanko
+        _ciufciuf.X += _ciufciuf.CiufciowaPredkosc;
+
+        // aktualizacja pozycji ciufci na canvas
+        Canvas.SetLeft(_ciufciuf.CiufciufImage, _ciufciuf.X);
+
+        // czy ciufcia wyjechała poza ekran
+        if ((_ciufciuf.Kieruneczek == 0 && _ciufciuf.X < -_ciufciufWidth) ||
+            (_ciufciuf.Kieruneczek == 1 && _ciufciuf.X > _tloDrogaWidth))
+        {
+            // zatrzymanie timera i usunięcie ciufci
+            _ciufciufTimer.Stop();
+            tloDroga.Children.Remove(_ciufciuf.CiufciufImage);
+            _ciufciuf = null;
+
+            // losowanie czasu przed kolejną ciufcią
+            int opuznienieCiuf = _random.Next(5, 11) * 1000;
+            Task.Delay(opuznienieCiuf).ContinueWith(_ => Dispatcher.Invoke(CiufciaJedzie));
         }
+    }
+    #endregion
+    #region Samochodzik
+    private void SamochodzikJedzie()
+    {
+        _samochodzik = new Samochodzik();
 
-        private void InitializeTimer()
+        tloDroga.Children.Add(_samochodzik.SamochodzikImage);
+
+        // Pobranie szerokości obszaru tła i samochodziku
+        _tloDrogaWidth = (int)tloDroga.Width;
+        _samochodzikWidth = (int)(_samochodzik.SamochodzikImage as FrameworkElement).Width;
+
+        // Ustawienie początkowej pozycji na Canvas
+        Canvas.SetLeft(_samochodzik.SamochodzikImage, _samochodzik.X);
+        Canvas.SetTop(_samochodzik.SamochodzikImage, _samochodzik.Y);
+
+        _samochodzikTimer.Start();
+    }
+
+
+    private void SamochodzikTimer_Tick(object sender, EventArgs e)
+    {
+        if (_samochodzik == null) return;
+
+        // Poruszanko
+        _samochodzik.X += _samochodzik.SamochodzikowaPredkosc;
+
+        // Aktualizacja pozycji samochodziku na Canvas
+        Canvas.SetLeft(_samochodzik.SamochodzikImage, _samochodzik.X);
+
+        // Czy samochodzik wyjechał poza ekran
+        if ((_samochodzik.X < -_samochodzikWidth && _samochodzik.Kieruneczek == 0) ||
+            (_samochodzik.X > _tloDrogaWidth && _samochodzik.Kieruneczek == 1))
         {
-            // Inicjalizacja timera
-            _ciufciufTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(30)
-            };
-            _ciufciufTimer.Tick += CiufciufTimer_Tick; //obsługa zdarzenia tick
+            // Zatrzymanie timera i usunięcie samochodziku
+            _samochodzikTimer.Stop();
+            tloDroga.Children.Remove(_samochodzik.SamochodzikImage);
+            _samochodzik = null;
+
+            // Losowanie czasu przed kolejnym samochodzikiem
+            int opoznienieSamochodzik = _random.Next(5, 11) * 1000;
+            Task.Delay(opoznienieSamochodzik).ContinueWith(_ => Dispatcher.Invoke(SamochodzikJedzie));
         }
+    }
 
-        private void CiufciaJedzie()
+    #endregion
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (_isMoving) return; //zapobiega wielokrotnemu uruchamianiu
+        _isMoving = true;
+        CiufciaJedzie();
+        SamochodzikJedzie();
+    }
+
+    private void StopButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ciufciufTimer.Stop();
+        _samochodzikTimer.Stop();
+        _isMoving = false;
+
+        // usunięcie istniejącej ciufci
+        if (_ciufciuf != null)
         {
-            _ciufciuf = new CiufCiuf();
-
-            tloCiuf.Children.Add(_ciufciuf.CiufciufRectangle);
-
-            //pobranie szerokości obszaru tła i ciufci
-            _tloCiufWidth = (int)tloCiuf.Width;
-            _ciufciufWidth = (int)(_ciufciuf.CiufciufRectangle as FrameworkElement).Width;
-
-            //ustawienie początkowej pozycji na Canvas
-            Canvas.SetLeft(_ciufciuf.CiufciufRectangle, _ciufciuf.X);
-            Canvas.SetTop(_ciufciuf.CiufciufRectangle, _ciufciuf.Y);
-
-            _ciufciufTimer.Start();
+            tloDroga.Children.Remove(_ciufciuf.CiufciufImage);
+            _ciufciuf = null;
         }
-
-        private void CiufciufTimer_Tick(object sender, EventArgs e)
+        // usunięcie istniejącej samochodu
+        if (_samochodzik != null)
         {
-            if (_ciufciuf == null) return;
-
-            //poruszanko
-            _ciufciuf.X += _ciufciuf.CiufciowaPredkosc;
-
-            // aktualizacja pozycji ciufci na Canvas
-            Canvas.SetLeft(_ciufciuf.CiufciufRectangle, _ciufciuf.X);
-
-            //czy ciufcia wyjechała poza ekran
-            if ((_ciufciuf.kieruneczek == 0 && _ciufciuf.X < -_ciufciufWidth) ||
-                (_ciufciuf.kieruneczek == 1 && _ciufciuf.X > _tloCiufWidth))
-            {
-                //zatrzymanie timera i usunięcie ciufci
-                _ciufciufTimer.Stop();
-                tloCiuf.Children.Remove(_ciufciuf.CiufciufRectangle);
-                _ciufciuf = null;
-
-                //losowanie czasu przed kolejną ciufcią
-                int opuznienieCiuf = _random.Next(5000, 10001);
-                Task.Delay(opuznienieCiuf).ContinueWith(_ => Dispatcher.Invoke(CiufciaJedzie));
-            }
-        }
-
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_isMoving) return; // Zapobiega wielokrotnemu uruchamianiu
-            _isMoving = true;
-            CiufciaJedzie();
-        }
-
-        private void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-            _ciufciufTimer.Stop(); // Zatrzymanie timera
-            _isMoving = false;
-
-            // Usunięcie ciufci, jeśli istnieje
-            if (_ciufciuf != null)
-            {
-                tloCiuf.Children.Remove(_ciufciuf.CiufciufRectangle);
-                _ciufciuf = null;
-            }
+            tloDroga.Children.Remove(_samochodzik.SamochodzikImage);
+            _samochodzik = null;
         }
     }
 }
